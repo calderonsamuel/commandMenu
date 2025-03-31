c55258775ddb55da70d5040935f9f80dafadb059
Samuel Calderon <samuel.calderon@uarm.pe>
2025-03-30 22:09:30 -0500

    add high level structure for command and commandMenu classes

---
+++ R/class-command.R
@@ -0,0 +1,28 @@
+command <- S7::new_class(
+  name = "command",
+  properties = list(
+    id = S7::class_character,
+    label = S7::class_character,
+    icon = NULL | S7::new_S3_class("html")
+  )
+)
+
+commandMenu <- S7::new_class(
+  name = "commandMenu",
+  properties = list(
+    commands = S7::new_property(
+      class = S7::class_list,
+      validator = function(value) {
+        if (length(value) < 1L) return("Must have at least one <command>")
+        are_all_commands <- value |>
+          lapply(function(x) S7::S7_inherits(x, command)) |>
+          unlist() |>
+          all()
+
+        if (!are_all_commands) return("Every element must be a <command>")
+      }
+    )
+  )
+)
+
+commandMenu(commands = list())
