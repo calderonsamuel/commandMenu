7658e9243248397eca23d272ce75a9e9cef5532a
Samuel Calderon <samuel.calderon@uarm.pe>
2025-04-06 16:32:19 -0500

    move check_input to its own file

--- R/class-command.R
+++ R/class-command.R
@@ -26,17 +26,3 @@ commandMenu <- S7::new_class(
 )
 
 add_command_menu <- S7::new_generic("add_command_menu", "x")
-
-check_input <- S7::new_generic("check_input", "x")
-
-S7::method(check_input, S7::new_S3_class("shiny.tag")) <- function(x, commandMenu) {
-  tq <- htmltools::tagQuery(x)
-  is_textarea_input <- tq$children("textarea.shiny-input-textarea")$length() == 1L
-  if (is_textarea_input) return()
-
-  tq$resetSelected()
-  is_text_input <- tq$children("input.shiny-input-text")$length() == 1L
-  if (is_text_input) return()
-
-  stop("Element should be a textInput or textAreaInput", call. = FALSE)
-}
