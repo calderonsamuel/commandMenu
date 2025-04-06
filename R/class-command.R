c7f80aa46888f645c47d2deb9340888ff4247759
Samuel Calderon <samuel.calderon@uarm.pe>
2025-04-06 10:41:49 -0500

    add methods to check if tag is textInput/textAreaInput

--- R/class-command.R
+++ R/class-command.R
@@ -25,4 +25,25 @@ commandMenu <- S7::new_class(
   )
 )
 
-commandMenu(commands = list())
+add_command_menu <- S7::new_generic("add_command_menu", "x")
+
+check_input <- S7::new_generic("check_input", "x")
+is_text_input <- S7::new_generic("is_text_input", "x")
+is_textarea_input <- S7::new_generic("is_textarea_input", "x")
+
+S7::method(check_input, S7::new_S3_class("shiny.tag")) <- function(x, commandMenu) {
+  tq <- htmltools::tagQuery(x)
+  if (is_textarea_input(tq)) return()
+  tq$resetSelected()
+  if (is_text_input(tq)) return()
+
+  stop("Element should be a textInput or textAreaInput")
+}
+
+S7::method(is_text_input, S7::new_S3_class("shiny.tag.query")) <- function(x) {
+  tq$children("input.shiny-input-text")$length() == 1L
+}
+
+S7::method(is_textarea_input, S7::new_S3_class("shiny.tag.query")) <- function(x) {
+  tq$children("textarea.shiny-input-textarea")$length() == 1L
+}
