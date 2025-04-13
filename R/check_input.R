f126172a19306c9d138ac9966b7e67bdbdb0f9b8
Samuel Calderon <samuel.calderon@uarm.pe>
2025-04-13 17:31:13 -0500

    return invisible() when check_input is successful

--- R/check_input.R
+++ R/check_input.R
@@ -3,11 +3,11 @@ check_input <- S7::new_generic("check_input", "x")
 S7::method(check_input, class_shiny.tag) <- function(x, command_menu) {
   tq <- htmltools::tagQuery(x)
   is_textarea_input <- tq$children("textarea.shiny-input-textarea")$length() == 1L
-  if (is_textarea_input) return()
+  if (is_textarea_input) return(invisible())
 
   tq$resetSelected()
   is_text_input <- tq$children("input.shiny-input-text")$length() == 1L
-  if (is_text_input) return()
+  if (is_text_input) return(invisible())
 
   stop("Element should be a textInput or textAreaInput", call. = FALSE)
 }
